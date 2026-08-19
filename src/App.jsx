eec2f1056b038064fa0d26309ed0f19cf516788e
soumik03 <soumik.datta2011@gmail.com>
2026-08-19 13:24:08 +0530

    Day13_changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home';
 import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
   const [theme, setTheme] = useState('light');
-
   useEffect(() => {
     document.body.dataset.theme = theme;
     localStorage.setItem('theme', theme);
@@ -33,6 +35,9 @@ function App() {
           <Routes>
           <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer />
